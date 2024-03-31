c76e6a5e23e29fe3a4a234ac4bf273069e2f4dfe
darkmewtwo <ashleysunil07@gmail.com>
2024-03-31 12:32:09 +0530

    added new path

--- app/server.go
+++ app/server.go
@@ -9,6 +9,19 @@ import (
 	"os"
 )
 
+func getHeaders(req_parts []string) map[string]string {
+	headers := make(map[string]string)
+
+	for _, line := range req_parts {
+		parts := strings.Split(line, ": ")
+		if len(parts) == 2 {
+			headers[parts[0]] = parts[1]
+		}
+	}
+
+	return headers
+}
+
 func handleConnection(conn net.Conn) {
 
 	var response []byte
@@ -18,19 +31,32 @@ func handleConnection(conn net.Conn) {
 	// fmt.Println(buffN)
 	request := string(buffer[:buffN])
 	// fmt.Println("REQUEST: ", request)
-	req_parts := strings.Split(request, " ")
+	req_parts := strings.Split(request, "\r\n")
+	// fmt.Println(req_parts)
+	req_path_method := strings.Split(req_parts[0], " ")
+	headers := getHeaders(req_parts)
+
+	// fmt.Println(headers, req_path_method)
 	// fmt.Println(req_parts, reflect.TypeOf(req_parts), req_parts[1])
-	if req_parts[1] == "/" {
+	if req_path_method[1] == "/" {
 		response = []byte("HTTP/1.1 200 OK\r\nContent-Length: 13\r\nContent-Type: text/plain\r\n\r\nHello, world!")
 
-	} else if strings.HasPrefix(req_parts[1], "/echo/") {
-		path := strings.Split(req_parts[1], "/echo/")
+	} else if strings.HasPrefix(req_path_method[1], "/echo/") {
+		path := strings.Split(req_path_method[1], "/echo/")
 		resource := path[len(path)-1]
 		// fmt.Println(resource)
 		res_len := len(resource)
 		message := "HTTP/1.1 200 OK\r\nContent-Length:" + fmt.Sprint(res_len) + "\r\nContent-Type: text/plain\r\n\r\n" + resource
 		response = []byte(message)
 
+	} else if strings.HasPrefix(req_path_method[1], "/user-agent") {
+		user_agent, exists := headers["User-Agent"]
+		if !exists {
+			response = []byte("HTTP/1.1 200 OK\r\nContent-Length: 25\r\nContent-Type: text/plain\r\n\r\nNo user-agent in headers")
+		} else {
+			message := "HTTP/1.1 200 OK\r\nContent-Length:" + fmt.Sprint(len(user_agent)) + "\r\nContent-Type: text/plain\r\n\r\n" + user_agent
+			response = []byte(message)
+		}
 	} else {
 		response = []byte("HTTP/1.1 404 Not Found\r\nContent-Length: 11\r\nContent-Type: text/plain\r\n\r\nNot Found")
 
